f9ed6a3b7cbf0926480dc27d000e989252832d2f
Luis Castillo <luis_castle_2011@hotmail.com>
2020-05-19 11:15:27 -0500

    Fin seccion 4"

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  titulo: string;
+  constructor(public infoPaginaService: InfoPaginaService) { }
 
   ngOnInit(): void {
+    this.titulo = this.infoPaginaService.info.titulo;
   }
 
 }
