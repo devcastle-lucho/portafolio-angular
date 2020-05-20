db33ffbf0ddf382228cfcb43598dca2f04e9b822
Luis Castillo <luis_castle_2011@hotmail.com>
2020-05-20 05:32:42 -0500

    Fin de la sección 6"

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { InfoPagina } from '../interfaces/info-pagina';
+import { InfoPagina } from '../interfaces/info-pagina.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +9,24 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
     .subscribe((resp: InfoPagina) => {
-
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
     });
-   }
+  }
+  private cargarEquipo() {
+    this.http.get('https://login-app-eb97f.firebaseio.com/equipo.json')
+    .subscribe((resp: any[]) => {
+      this.equipo = resp;
+    });
+  }
 }
